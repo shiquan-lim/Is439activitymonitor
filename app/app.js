8e9d17a3c5fdb4cd13424605e6aeff279b8aee27
shiquan-lim <shiquan.lim@gmail.com>
2016-08-30 15:03:36 +0800

    Undo breaks in libraries

--- app/app.js
+++ app/app.js
@@ -1,10 +1,26 @@
 import {App, Platform} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {TabsPage} from './pages/tabs/tabs';
+// import { FIREBASE_PROVIDERS,
+//     defaultFirebase,
+//     AngularFire,
+//     AuthMethods,
+//     AuthProviders,
+//     firebaseAuthConfig } from 'angularfire2';
+import { ionicBootstrap, Storage, LocalStorage, NavController } from 'ionic-angular';
 
 
 @App({
   template: '<ion-nav [root]="rootPage"></ion-nav>',
+  // providers: [
+  //     FIREBASE_PROVIDERS,
+  //     defaultFirebase({
+  //         apiKey: "AIzaSyA0QSxxxBURiGZqIAiUl45FXc_iyUDNXU8",
+  //         authDomain: "is439activitymonitor.firebaseapp.com",
+  //         databaseURL: "https://is439activitymonitor.firebaseio.com",
+  //         storageBucket: "is439activitymonitor.appspot.com",
+  //     })
+  // ],
   config: {} // http://ionicframework.com/docs/v2/api/config/Config/
 })
 export class MyApp {
--- app/pages/page1/page1.js
+++ app/pages/page1/page1.js
@@ -1,11 +1,116 @@
 import {Page} from 'ionic-angular';
-
+// import {Component} from '@angular/core';
 
 @Page({
   templateUrl: 'build/pages/page1/page1.html'
 })
-export class Page1 {
-  constructor() {
 
+export class Page1 {
+  // options;
+  // testline;
+  constructor() {
+      // this.items = [
+      //     'Amsterdam',
+      //     'Bogota',
+      //     'Singapore'
+      // ];
+      // this.testline = "Hello controller!";
+      // this.options = {
+      //     chart: {
+      //         type: "pieChart",
+      //         height: 500,
+      //         x: function(d){return d.key;},
+      //         y: function(d){return d.y;},
+      //         showLabels: true,
+      //         duration: 500,
+      //         labelThreshold: 0.01,
+      //         labelSunbeamLayout: true,
+      //         legend: {
+      //             margin: {
+      //                 top: 5,
+      //                     right: 35,
+      //                     bottom: 5,
+      //                     left: 0
+      //             }
+      //         }
+      //     }
+      // };
+      // this.data = [
+      //     {
+      //         key: 'One',
+      //         y: 5
+      //     },
+      //     {
+      //         key: 'Two',
+      //         y: 2
+      //     },
+      //     {
+      //         key: 'Three',
+      //         y: 9
+      //     },
+      //     {
+      //         key: 'Four',
+      //         y: 7
+      //     },
+      //     {
+      //         key: 'Five',
+      //         y: 4
+      //     },
+      //     {
+      //         key: 'Six',
+      //         y: 3
+      //     },
+      //     {
+      //         key: 'Seven',
+      //         y: .5
+      //     }
+      // ];
   }
+  //   items;
+  //
+  //   constructor() {
+  //       this.initializeItems();
+  //   }
+  //
+  //   initializeItems() {
+  //       this.items = [
+  //           'Amsterdam',
+  //           'Bogota',
+  //           'Buenos Aires',
+  //           'Cairo',
+  //           'Dhaka',
+  //           'Edinburgh',
+  //           'Geneva',
+  //           'Genoa',
+  //           'Glasglow',
+  //           'Hanoi',
+  //           'Hong Kong',
+  //           'Islamabad',
+  //           'Istanbul',
+  //           'Jakarta',
+  //           'Kiel',
+  //           'Kyoto',
+  //           'Le Havre',
+  //           'Lebanon',
+  //           'Lhasa',
+  //           'Lima',
+  //           'London',
+  //           'Los Angeles',
+  //           'Madrid',
+  //           'Manila',
+  //           'New York',
+  //           'Olympia',
+  //           'Oslo',
+  //           'Panama City',
+  //           'Peking',
+  //           'Philadelphia',
+  //           'San Francisco',
+  //           'Seoul',
+  //           'Taipeh',
+  //           'Tel Aviv',
+  //           'Tokio',
+  //           'Uelzen',
+  //           'Washington'
+  //       ];
+  //   }
 }
