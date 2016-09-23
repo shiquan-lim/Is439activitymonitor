7a40fe4fa483e87e901c8bad2123eafb70476b82
shiquan-lim <shiquan.lim@gmail.com>
2016-09-23 22:23:11 +0800

    Added nvd3 and d3 dependencies

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 import {App, Platform} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {TabsPage} from './pages/tabs/tabs';
+
 // import { FIREBASE_PROVIDERS,
 //     defaultFirebase,
 //     AngularFire,
